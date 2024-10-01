becce08d3fd299b74f21efef5c8e3e0c25e46126
Felipe Quintella <felipe@quintella.com>
2024-10-01 08:09:51 -0300

    inc

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,4 +8,8 @@ class fluentbit::install {
     ensure => $fluentbit::package_ensure,
     name   => $fluentbit::package_name,
   }
+  ->  file { '/usr/local/bin/fluent-bit':
+    ensure => 'link',
+    target => '/opt/fluent-bit/bin/fluent-bit',
+  }
 }
